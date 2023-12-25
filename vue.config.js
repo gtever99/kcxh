b32c4484c0801d6b3b3a8462d836c62aad45a3fe
高振强 <1933669775@qq.com>
2023-12-25 14:34:03 +0800

    创建项目

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,30 @@
 const { defineConfig } = require("@vue/cli-service");
+const AutoImport = require("unplugin-auto-import/webpack");
+const Components = require("unplugin-vue-components/webpack");
+const { ElementPlusResolver } = require("unplugin-vue-components/resolvers");
+
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
+  devServer: {
+    proxy: {
+      "/api_": {
+        target: "http://10.8.1.3:8088",
+        changeOrigin: true,
+        pathRewrite: {
+          "^/api_": ""
+        }
+      }
+    }
+  },
+  configureWebpack: {
+    plugins: [
+      AutoImport({
+        resolvers: [ElementPlusResolver({ importStyle: "sass" })]
+      }),
+      Components({
+        resolvers: [ElementPlusResolver({ importStyle: "sass" })]
+      })
+    ]
+  }
 });
